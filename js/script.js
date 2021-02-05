9fbaf5543ab1231f73dd1ca5fb903168f0496798
Boldinov90 <detroid90@mail.ru>
2021-02-05 20:17:54 +0300

    s90

--- js/script.js
+++ js/script.js
@@ -23,26 +23,77 @@ for(let i = 0; i < call.length; i++){
 }
 
 // =========== Навигация по сайту =============
-const anchors = document.querySelectorAll('a[href*= "#"]')
-for(let anchor of anchors){
-   anchor.addEventListener("click", function(event){
-      event.preventDefault();
-      const blockId = anchor.getAttribute("href");
-      document.querySelector('' + blockId).scrollIntoView({
-         behavior: "smooth",
-         block: "start" 
-      })
-      mobileNavContainer.classList.remove('mobile-nav--active');
-      menuToggle.classList.remove('menu-icon-active');
-      let active = document.querySelector(".header__menu--active");
-      if(active == null){
-         anchor.classList.add("header__menu--active")
-      }else{
-         active.classList.remove("header__menu--active");
-         anchor.classList.add("header__menu--active");
-      }
-   })
-}
+// const anchors = document.querySelectorAll('a[href*= "#"]')
+// for(let anchor of anchors){
+//    anchor.addEventListener("click", function(event){
+//       event.preventDefault();
+//       const blockId = anchor.getAttribute("href");
+//       document.querySelector('' + blockId).scrollIntoView({
+//          behavior: "smooth",
+//          block: "start" 
+//       })
+//       mobileNavContainer.classList.remove('mobile-nav--active');
+//       menuToggle.classList.remove('menu-icon-active');
+//       let active = document.querySelector(".header__menu--active");
+//       if(active == null){
+//          anchor.classList.add("header__menu--active")
+//       }else{
+//          active.classList.remove("header__menu--active");
+//          anchor.classList.add("header__menu--active");
+//       }
+//    })
+// }
+
+$(document).ready(function(){
+   // Добавить плавную прокрутку до всех ссылок
+   $("a").on('click', function(event) {
+     // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
+     if (this.hash !== "") {
+       // Запретить поведение щелчка якоря по умолчанию
+       event.preventDefault();
+       // Хранить хэш
+       var hash = this.hash;
+       // Использование метода animate() jQuery для добавления плавной прокрутки страницы
+       // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
+       $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 500, function(){
+         // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
+         window.location.hash = hash;
+       });
+       mobileNavContainer.classList.remove('mobile-nav--active');
+       document.querySelector('body').classList.remove("no-scroll");
+       menuToggle.classList.remove('menu-icon-active');
+     } // Конец, если
+   });
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Изменение активного класса навигации при скроле
 window.addEventListener("scroll", ()=>{
