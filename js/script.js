49ed0471b5b21a5ef24a15018304d05e264b1ce4
Boldinov90 <detroid90@mail.ru>
2021-02-05 22:55:05 +0300

    s90

--- js/script.js
+++ js/script.js
@@ -47,26 +47,33 @@ for(let i = 0; i < call.length; i++){
 $(document).ready(function(){
    // Добавить плавную прокрутку до всех ссылок
    $("a").on('click', function(event) {
-     // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
-     if (this.hash !== "") {
-       // Запретить поведение щелчка якоря по умолчанию
-       event.preventDefault();
-       // Хранить хэш
-       var hash = this.hash;
-       // Использование метода animate() jQuery для добавления плавной прокрутки страницы
-       // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
-       $('html, body').animate({
-         scrollTop: $(hash).offset().top
-       }, 500, function(){
-         // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
-         window.location.hash = hash;
-       });
-       mobileNavContainer.classList.remove('mobile-nav--active');
-       document.querySelector('body').classList.remove("no-scroll");
-       menuToggle.classList.remove('menu-icon-active');
-     } // Конец, если
+      // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
+      if (this.hash !== "") {
+         // Запретить поведение щелчка якоря по умолчанию
+         event.preventDefault();
+         // Хранить хэш
+         var hash = this.hash;
+         // Использование метода animate() jQuery для добавления плавной прокрутки страницы
+         // Необязательное число (500) указывает количество миллисекунд, необходимых для прокрутки до указанной области
+         $('html, body').animate({
+            scrollTop: $(hash).offset().top
+         }, 500, function(){
+            // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
+            window.location.hash = hash;
+         });
+         mobileNavContainer.classList.remove('mobile-nav--active');
+         document.querySelector('body').classList.remove("no-scroll");
+         menuToggle.classList.remove('menu-icon-active');
+         let active = document.querySelector(".header__menu--active");
+         if(active == null){
+            anchor.classList.add("header__menu--active")
+         }else{
+            active.classList.remove("header__menu--active");
+            anchor.classList.add("header__menu--active");
+         }
+      } // Конец, если
    });
- });
+});
 
 
 
